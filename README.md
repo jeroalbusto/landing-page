f19f444d250cb8065b888b1573371fdd0e841d37
jeronimo <jeroalbusto00@gmail.com>
2024-09-18 16:56:52 -0300

    initial commit 🎉

---
+++ README.md
@@ -0,0 +1,4 @@
+# Landing Page
+
+React v18.3.1
+Vite v5.4.1
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl uppercase'>Welcome to Project</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
