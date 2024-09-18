ca18534a07a3f2cc34ab6eafaa2b2413aa66f05c
jeronimo <jeroalbusto00@gmail.com>
2024-09-18 17:42:35 -0300

    feat: se creo un componente navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
+import NavBAR from './components/NavBar'
 
 function App() {
   return (
     <>
-      <div>
-        <h1 className='text-3xl uppercase'>Welcome to Project</h1>
-      </div>
+    <NavBAR />
     </>
   )
 }
