4c6420df90c3f5498962251ee65a082851d5026a
jeronimo <jeroalbusto00@gmail.com>
2024-09-19 20:26:22 -0300

    feat: se creo un componente heroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import NavBAR from './components/NavBar'
+import HeroSection from './components/HeroSection'
 
 function App() {
   return (
     <>
     <NavBAR />
+    <div className='container mx-auto'>
+    <HeroSection />
+    </div>
     </>
   )
 }
